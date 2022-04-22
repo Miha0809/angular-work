40df44352bcdd590c34d19172add87eea2873e68
Miha0809 <40777154+Miha0809@users.noreply.github.com>
2022-04-22 12:49:08 +0300

    V2
    Change architecture project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ApplicantComponent } from './components/applicant/applicant.component';
+import { EmployerComponent } from './components/employer/employer.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
+import { SignInComponent } from './components/authorization/sign-in/sign-in.component';
+import { SignUpComponent } from './components/authorization/sign-up/sign-up.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', component: ApplicantComponent},
+  {path: "employer", component: EmployerComponent},
+  {path: "authorization", component: AuthorizationComponent, children: [
+    {path: "sign-in", component: SignInComponent},
+    {path: "sign-up", component: SignUpComponent}
+  ]},
+  {path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routesComponents = [
+  ApplicantComponent,
+  EmployerComponent,
+  AuthorizationComponent,
+  SignInComponent,
+  SignUpComponent,
+  PageNotFoundComponent
+];
